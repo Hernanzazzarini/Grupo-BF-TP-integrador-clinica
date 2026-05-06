542fee0662f47bc206f140bceaf642b39fb26361
fabianagustinromerodegreef-dotcom <fabianagustinromerodegreef@gmail.com>
2026-05-06 11:19:40 -0300

    Update database connection configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ dotenv.config();
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // vacio
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  password: process.env.DB_PASSWORD,  // Sin el = agustin19
+  database: process.env.DB_NAME,       // Esto leerá 'prog3_clinica'
+  port: parseInt(process.env.DB_PORT)
 });
